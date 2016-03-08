67654cc4e7ff83f6892dc828abb635f58fc2e4fe
Shaswat27 <shaswatdatta@hotmail.com>
2016-03-08 07:16:32 +0530

    OpenCV C++ integrated to convert frames to grayscale using native code

---
+++ TestOpenCV/app/src/main/obj/local/arm64-v8a/objs/nativegray/jni_part.o.d
@@ -0,0 +1,175 @@
+obj/local/arm64-v8a/objs/nativegray/jni_part.o: jni/jni_part.cpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/limits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/limits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/linux/limits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/syslimits.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/stdint.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/machine/wchar_limits.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/assert.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs/imgcodecs_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio/videoio_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp \
+ jni/com_example_shaswat_testopencv_MainActivity.h
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/limits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/limits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/linux/limits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/syslimits.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/stdint.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/machine/wchar_limits.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/assert.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs/imgcodecs_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio/videoio_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp:
+
+jni/com_example_shaswat_testopencv_MainActivity.h:
---
+++ TestOpenCV/app/src/main/obj/local/mips/objs/nativegray/jni_part.o.d
@@ -0,0 +1,187 @@
+obj/local/mips/objs/nativegray/jni_part.o: jni/jni_part.cpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/jni.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/cdefs.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/cdefs_elf.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/android/api-level.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/limits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/limits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/linux/limits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/machine/internal_types.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/machine/limits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/syslimits.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/asm/page.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/stdint.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/_types.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/machine/_types.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/climits \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/assert.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs/imgcodecs_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio/videoio_c.h \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp \
+ jni/com_example_shaswat_testopencv_MainActivity.h
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/jni.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/cdefs.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/cdefs_elf.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/android/api-level.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/limits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/limits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/linux/limits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/machine/internal_types.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/machine/limits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/syslimits.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/asm/page.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/stdint.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/sys/_types.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/machine/_types.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/climits:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/home/shaswat/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/shaswat/Android/Sdk/ndk-bundle/platforms/android-15/arch-mips/usr/include/assert.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs/imgcodecs_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio/videoio_c.h:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/home/shaswat/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp:
+
+jni/com_example_shaswat_testopencv_MainActivity.h:
